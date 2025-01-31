3fd679ea4e7b85b2f0fdc58f583e917d22d58923
Max Figs <89504574+Maxflowyoga@users.noreply.github.com>
2025-01-31 13:50:26 -0800

    add aws kinesis template and xrp txns class

---
+++ java/XRPL_API_Engine/src/main/java/com/xrpyoga/xrpledger/XRP_Ledger_Txns.java
@@ -0,0 +1,31 @@
+package com.xrpyoga.xrpledger;
+
+import org.xrpl.xrpl4j.client.JsonRpcClientErrorException;
+
+public class XRP_Ledger_Txns {
+	
+	
+	
+	
+	
+	// Send XRP methods
+	
+	
+	public Object sendTestnetXRP() throws JsonRpcClientErrorException  {
+		
+		Object returnResult = new Object();
+		
+		
+		
+		
+		return returnResult;
+		
+	}
+	
+	
+	
+	
+	
+	// End Send XRP methods
+
+}

8627ebc3fcbd497d455d64319f26e9d488b1e0cc
Max Figs <89504574+Maxflowyoga@users.noreply.github.com>
2025-01-31 21:15:11 -0800

    add MPT multiPurposeToken CRUD and clawback

---
+++ java/XRPL_API_Engine/src/main/java/com/xrpl/xrpledger/XRP_Ledger_MPToken_CRUD.java
@@ -0,0 +1,43 @@
+package com.xrpl.xrpledger;
+
+public class XRP_Ledger_MPToken_CRUD {
+	
+	
+	
+	
+	
+	
+	public boolean clawbackMPTokenCheck() {
+		
+		boolean isClawback = false;
+		
+		
+		//logic to check the tfMPTCanClawback flag
+		
+		
+		return isClawback;
+		
+	}
+	
+	
+	// Multi-purpose Token Authorize
+	
+	
+	
+	
+	// Multi-purpose Token Issuance Create
+	
+	
+	
+	
+	// Multi-purpose Token Issuance Destroy
+	
+	
+	
+	
+	// Multi-purpose Token Issuance Set
+	
+	
+	
+
+}

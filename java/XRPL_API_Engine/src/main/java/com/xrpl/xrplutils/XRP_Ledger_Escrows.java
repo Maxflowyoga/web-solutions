db57f04258707b43a53d23607d33b367882dabde
Max Figs <89504574+Maxflowyoga@users.noreply.github.com>
2025-02-04 22:28:56 -0800

    update escrow creation method

--- java/XRPL_API_Engine/src/main/java/com/xrpl/xrplutils/XRP_Ledger_Escrows.java
+++ java/XRPL_API_Engine/src/main/java/com/xrpl/xrplutils/XRP_Ledger_Escrows.java
@@ -1,13 +1,47 @@
 package com.xrpl.xrplutils;
 
+import org.xrpl.xrpl4j.model.transactions.Address;
+import org.xrpl.xrpl4j.model.transactions.EscrowCreate;
+import org.xrpl.xrpl4j.model.transactions.XrpCurrencyAmount;
 
-
-
-
+import com.google.common.io.BaseEncoding;
+import com.google.common.primitives.UnsignedInteger;
+import com.google.common.primitives.UnsignedLong;
+import com.ripple.cryptoconditions.CryptoConditionReader;
 
 public class XRP_Ledger_Escrows {
 	
 	
+	public EscrowCreate buildEscrow() {
+		
+		EscrowCreate escrowCreate = null;
+		
+		try {
+			
+			escrowCreate = EscrowCreate.builder() 
+					.account(Address.of("publicKey"))
+					.fee(XrpCurrencyAmount.ofDrops(12))
+					.sequence(null)
+					.amount(null)
+					.destination(Address.of("publicKey_Destination"))
+					.destinationTag(UnsignedInteger.valueOf(23480))
+					.cancelAfter(UnsignedLong.valueOf(533257958))
+					.finishAfter(UnsignedLong.valueOf(533171558))
+					.condition(CryptoConditionReader.readCondition(
+							BaseEncoding.base16()
+								.decode(""))
+							)
+							.sourceTag(UnsignedInteger.valueOf(11747))	
+							.build();
+			
+			return escrowCreate;
+			
+		} catch (Exception e) {
+			System.out.println("Error in Escrow Creation: " + e);
+		}
+		
+		return escrowCreate;
+	}
 	
 	
 	public void getTimeHeldEscrow() {

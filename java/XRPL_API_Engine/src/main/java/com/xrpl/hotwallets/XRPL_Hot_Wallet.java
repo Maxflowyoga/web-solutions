54d10c8d10f3b587d362242772a6fedbd4a718ea
Max Figs <89504574+Maxflowyoga@users.noreply.github.com>
2025-02-17 20:06:42 -0800

    add hot wallet class and NFTokenMintOffer amendment util class

---
+++ java/XRPL_API_Engine/src/main/java/com/xrpl/hotwallets/XRPL_Hot_Wallet.java
@@ -0,0 +1,22 @@
+package com.xrpl.hotwallets;
+
+
+
+public class XRPL_Hot_Wallet {
+
+	
+	
+	
+	
+}
+
+
+/*
+
+
+
+
+
+
+
+*/
---
+++ java/XRPL_API_Engine/src/main/java/com/xrpl/nfts/XRPL_NFT_NFTokenMintOffer.java
@@ -0,0 +1,14 @@
+package com.xrpl.nfts;
+
+// Amendment: NFTokenMintOffer
+/*
+ * adds three optional fields to the transaction, which definte the NFT sell offer if your provide them:
+ * - Amount, The Sell price of the NFT
+ * - Destination, Limit the sale so that it can only be accepted by this account
+ * - Expiration, The Time after which this sell offer expires 
+ * 
+ * 
+ */
+public class XRPL_NFT_NFTokenMintOffer {
+
+}
